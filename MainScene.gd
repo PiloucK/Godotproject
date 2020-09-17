8baac4969a6d7823f3188e580b3e1c50196f3e3a
Julien Maydew <MDW@MacBook-Pro-de-Julien.local>
2020-09-17 11:47:42 +0200

    a

--- MainScene.gd
+++ MainScene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var round_results = [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]]
+var round_results = [[0,-1,0,2], [-1,0,1,1], [-1,-1,0,-1],[1,1,1,0]]
 var total_score = [0, 0, 0, 0]
 
 onready var voting_timer = $VotingTimer
@@ -30,15 +30,19 @@ func compute_round_results():
 	var j = 0
 
 	while i < 4:
-		j = 0
+		j = i
 		while j < 4:
 			if round_results[i][j] < round_results[j][i]: #You cheat
 				round_results[i][j] = 3
+				round_results[j][i] = -1
 			elif round_results[i][j] > round_results[j][i]: #They cheated
 				round_results[i][j] = -1
+				round_results[j][i] = 3
 			elif (round_results[i][j] == 1) && (round_results[j][i] == 1): #Cooperation
 				round_results[i][j] = 2
+				round_results[i][j] = 2
 			elif (round_results[i][j] == -1) && (round_results[j][i] == -1): #Both cheat
+				round_results[i][j] = 0
 				round_results[i][j] = 0
 			j += 1
 		i += 1
