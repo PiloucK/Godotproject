d40ae24557b29e89b2e6a60ddd3c10fee3220fea
KuzniClem <clement@kuzniewycz.fr>
2020-09-18 23:27:03 +0200

    .

--- ForthPlace.gd
+++ ForthPlace.gd
@@ -3,28 +3,30 @@ extends Node2D
 func show_total_score_window():
 	var total_score = NeededValues.total_score
 	var ranking = total_score
-	ranking.sort()
-	ranking.invert()
 	var i = 0
 	$RedFace.hide()
 	$GreenFace.hide()
 	$BlueFace.hide()
 	$PurpleFace.hide()
+	var best_score = ranking[0]
+	var p = 0
+	i += 1
 	while i < 4:
-		if ranking[3] == total_score[i] :
+		if (ranking[i] < best_score):
 			if get_parent().score_table[i] == 0:
-				if i == 0:
-					$RedFace.show()
-					get_parent().score_table[0] = 1
-				elif i == 1:
-					$GreenFace.show()
-					get_parent().score_table[1] = 1
-				elif i == 2:
-					$BlueFace.show()
-					get_parent().score_table[2] = 1
-				elif i == 3:
-					$PurpleFace.show()
-					get_parent().score_table[3] = 1
-				$Score.text = str(ranking[i])
-				break
+				best_score = ranking[i]
+				p = i
 		i += 1
+	if get_parent().score_table[p] == 0:
+		if p == 0:
+			$RedFace.show()
+		elif p == 1:
+			$GreenFace.show()
+		elif p == 2:
+			$BlueFace.show()
+		elif p == 3:
+			$PurpleFace.show()
+		get_parent().score_table[p] = 1
+		$Score.text = str(best_score)
+	print("RANKING: ")
+	print(ranking)
